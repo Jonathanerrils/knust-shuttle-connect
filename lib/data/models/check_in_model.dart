bde585de5ea006f1656ef04e8b50477024fbba73
Jonathanerrils <100030343+Jonathanerrils@users.noreply.github.com>
2026-08-27 11:32:34 +0100

    chore: migrate check-in Firestore model

---
+++ lib/data/models/check_in_model.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../domain/entities/check_in.dart';
+
+class CheckInModel {
+  CheckInModel._();
+
+  static CheckIn? fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final data = doc.data();
+    if (data == null) return null;
+    return CheckIn(
+      studentUid: doc.id,
+      stopId: data['stopId'] as String,
+      stopName: (data['stopName'] as String?) ?? '',
+      createdAt: (data['createdAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+      expiresAt: (data['expiresAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+    );
+  }
+}
