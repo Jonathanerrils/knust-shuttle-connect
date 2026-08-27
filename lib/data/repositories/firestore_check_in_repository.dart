9e090ada0e4ec5ac94c914c7db10ab3b2173373b
Jonathanerrils <100030343+Jonathanerrils@users.noreply.github.com>
2026-08-27 11:33:11 +0100

    chore: migrate check-in repository

---
+++ lib/data/repositories/firestore_check_in_repository.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../core/constants/app_constants.dart';
+import '../../domain/entities/bus_stop.dart';
+import '../../domain/entities/check_in.dart';
+import '../../domain/repositories/check_in_repository.dart';
+import '../models/check_in_model.dart';
+
+class FirestoreCheckInRepository implements CheckInRepository {
+  final FirebaseFirestore _db;
+
+  FirestoreCheckInRepository(this._db);
+
+  DocumentReference<Map<String, dynamic>> _doc(String uid) =>
+      _db.collection('checkins').doc(uid);
+
+  @override
+  Stream<CheckIn?> watchMyCheckIn(String uid) => _doc(uid).snapshots().map(
+        (doc) {
+          final checkIn = CheckInModel.fromDoc(doc);
+          if (checkIn == null || checkIn.isExpired) return null;
+          return checkIn;
+        },
+      );
+
+  @override
+  Future<void> checkIn({required String uid, required BusStop stop}) {
+    return _doc(uid).set(<String, dynamic>{
+      'stopId': stop.id,
+      'stopName': stop.name,
+      'createdAt': FieldValue.serverTimestamp(),
+      'updatedAt': FieldValue.serverTimestamp(),
+      'expiresAt': Timestamp.fromDate(
+        DateTime.now().add(AppConstants.checkInTtl),
+      ),
+    });
+  }
+
+  @override
+  Future<void> cancel(String uid) => _doc(uid).delete();
+}
