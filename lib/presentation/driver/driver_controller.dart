da23fb69fb5fc28981de18702bb6d8c84ce7f6dd
Jonathanerrils <100030343+Jonathanerrils@users.noreply.github.com>
2026-08-27 11:38:01 +0100

    chore: migrate driver controller

---
+++ lib/presentation/driver/driver_controller.dart
@@ -0,0 +1,98 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../../core/constants/app_constants.dart';
+import '../../domain/entities/app_user.dart';
+import '../../domain/entities/bus_stop.dart';
+import '../../domain/repositories/stop_repository.dart';
+
+class DriverController extends ChangeNotifier {
+  final AppUser driver;
+  final StopRepository _stops;
+  final FirebaseFirestore _db;
+
+  StreamSubscription<List<BusStop>>? _stopsSub;
+  StreamSubscription<Position>? _dutySub;
+
+  List<BusStop> stops = const [];
+  bool sharingLocation = false;
+  DateTime lastUpdate = DateTime.now();
+
+  DriverController({
+    required this.driver,
+    required StopRepository stopRepository,
+    FirebaseFirestore? db,
+  })  : _stops = stopRepository,
+        _db = db ?? FirebaseFirestore.instance {
+    _stopsSub = _stops.watchStops().listen((live) {
+      stops = [...live]
+        ..sort((a, b) => b.waitingCount.compareTo(a.waitingCount));
+      lastUpdate = DateTime.now();
+      notifyListeners();
+    });
+  }
+
+  bool isMine(BusStop stop) => stop.enRouteBy == driver.uid;
+
+  Future<void> markEnRoute(BusStop stop) =>
+      _stops.markEnRoute(stop.id, driver.uid);
+
+  Future<void> markArrived(BusStop stop) =>
+      _stops.markArrived(stop.id, driver.uid);
+
+  Future<void> clearEnRoute(BusStop stop) => _stops.clearEnRoute(stop.id);
+
+  Future<void> setSharingLocation(bool enabled) async {
+    if (enabled == sharingLocation) return;
+    if (!enabled) {
+      await _dutySub?.cancel();
+      _dutySub = null;
+      sharingLocation = false;
+      notifyListeners();
+      await _shuttleDoc.set(<String, dynamic>{
+        'onDuty': false,
+        'updatedAt': FieldValue.serverTimestamp(),
+      }, SetOptions(merge: true));
+      return;
+    }
+
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      return;
+    }
+    sharingLocation = true;
+    notifyListeners();
+    _dutySub = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: AppConstants.driverDistanceFilterMeters,
+      ),
+    ).listen((pos) {
+      unawaited(_shuttleDoc.set(<String, dynamic>{
+        'onDuty': true,
+        'latitude': pos.latitude,
+        'longitude': pos.longitude,
+        'heading': pos.heading,
+        'speed': pos.speed,
+        'updatedAt': FieldValue.serverTimestamp(),
+      }, SetOptions(merge: true)));
+    });
+  }
+
+  DocumentReference<Map<String, dynamic>> get _shuttleDoc =>
+      _db.collection('shuttles').doc(driver.uid);
+
+  @override
+  void dispose() {
+    _stopsSub?.cancel();
+    _dutySub?.cancel();
+    super.dispose();
+  }
+}
